a83564482aa8d8623cdc60769532deb0b8a79f60
abdallahhkaram <58476701+abdallahhkaram@users.noreply.github.com>
2021-02-01 14:43:48 +0300

    commit 1
    first push

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+window.onscroll = function () {
+    stickHead();
+};
+
+var header = document.getElementById("topHeader");
+var sticky = header.offsetTop;
+
+function stickHead() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function menuOnClick() {
+    document.getElementById("ham").classList.toggle("change");
+    document.getElementById("hamLinks").classList.toggle("ham-link-close");
+}
+
+// Get the modal
+var modal = document.getElementById("loginModal");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        document.body.style.overflow = "visible";
+        modal.style.display = "none";
+    }
+};
