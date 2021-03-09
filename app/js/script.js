aba69fb0fb287a627491e76ebefa32ae05765f21
abdallahhkaram <58476701+abdallahhkaram@users.noreply.github.com>
2021-03-09 12:21:21 +0300

    major update

--- app/js/script.js
+++ app/js/script.js
@@ -3,23 +3,12 @@ $("#info").load("./app/html/info.html");
 $("#footer").load("./app/html/footer.html");
 
 $(window).on("load", function () {
-    $(".loader-wrapper").delay(1000).fadeOut("slow");
+    $(".loader-wrapper").delay(100).fadeOut("slow");
 });
 function menuOnClick() {
     document.getElementById("ham").classList.toggle("change");
     document.getElementById("hamLinks").classList.toggle("ham-link-close");
 }
-
-// Get the modal
-var modal = document.getElementById("loginModal");
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        document.body.style.overflow = "visible";
-        modal.style.display = "none";
-    }
-};
 
 //////// lang selectopr
 function mobileDrop() {
